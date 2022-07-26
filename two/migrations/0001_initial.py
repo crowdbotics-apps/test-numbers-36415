e0c8986f651da3bde159a8aef358781fe6d34712
Crowdbotics <team@crowdbotics.com>
2022-07-26 02:55:59 +0000

    Update models

--- two/migrations/0001_initial.py
+++ two/migrations/0001_initial.py
@@ -7,15 +7,22 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Two',
+            name="Two",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('two', models.BigIntegerField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("two", models.BigIntegerField()),
             ],
         ),
     ]
