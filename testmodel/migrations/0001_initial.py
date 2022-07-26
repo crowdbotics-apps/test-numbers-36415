f91d87103a4549b033b406e1f9974ac12124e80a
Crowdbotics <team@crowdbotics.com>
2022-07-26 02:58:50 +0000

    Update models

--- testmodel/migrations/0001_initial.py
+++ testmodel/migrations/0001_initial.py
@@ -7,15 +7,22 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Testmodel',
+            name="Testmodel",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('testmodel', models.BigIntegerField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("testmodel", models.BigIntegerField()),
             ],
         ),
     ]
